1ff2485df4e431881c22b46ae21379e66bc0da47
Serik <66216464+Prozorog@users.noreply.github.com>
2021-04-17 23:51:29 +0600

    fixed some problem
    i fixed  API problems

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,7 +4,7 @@ import { MovieList } from "../components/MovieList";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
 
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
   state = {
@@ -13,7 +13,7 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch("https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix")
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((resp) => resp.json())
       .then((data) =>
         this.setState({
